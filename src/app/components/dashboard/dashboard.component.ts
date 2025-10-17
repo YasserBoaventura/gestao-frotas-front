9e843ab941773f8cfff81f20cccac0438287157b
Yasser Boaventura <yasserboaventura78@gmail.com>
2025-10-17 07:33:06 +0100

    autenticacao works

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -13,16 +13,20 @@ export class DashboardComponent {
   router=inject(Router);
 
     // Exemplo de dados da frota
+ 
   vehiclesAvailable = 12;
   vehiclesInUse = 5;
   vehiclesMaintenance = 3;
   upcomingRoutes = 4;
 
+  fuelEntriesToday = 6;  // registros de abastecimento do dia
+  fuelTotalLiters = 420; // total de litros abastecidos
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+// implementar componentes para essa rotas 
   navigateTo(path: string) {
     this.router.navigate([path]);
   }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,8 +3,9 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-
+import { LoginService } from '../auth/login.service';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Login } from '../auth/login';
 
 @Component({
   selector: 'app-login',
@@ -17,23 +18,28 @@ import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 })
 export class LoginComponent  {
 
-    router=inject(Router)
+constructor(){}  
+  
+   router=inject(Router)
 
-
-  login = {
-    username: '',
-    password: ''
-  };
+    login: Login =new Login();
+  
+  LoginService = inject (LoginService);
 
   logar() {
-  if(this.login.username=="admin" && this.login.password=="admin"){
-
+   this.LoginService.logar(this.login).subscribe({
+   next: token =>{
+   if(token){
+      this.LoginService.addToken(token);
     this.router.navigate(['dashboard']);
-   }else{
-    alert("Usuario e senha nao encontrados");
-   }
+      }}, 
+   error: erro =>{
+     alert("erro ao logar"+ erro);
+     console.log(erro);
+     
+   } 
 
-  }
+  })}
 
   forgotPassword() {
     console.log('Recuperar senha');
